7272a3d9680a0bf9ed39bfbf5f3bb348b2269751
Giannis Tolios <derevirn@gmail.com>
2021-08-24 16:16:48 +0300

    Update app.py

--- Deploying/app.py
+++ Deploying/app.py
@@ -1,7 +1,8 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 from pycaret.regression import load_model, predict_model
+
+st.set_page_config(page_title="Insurance Charges Prediction App")
 
 @st.cache(allow_output_mutation=True)
 def get_model():
@@ -13,26 +14,30 @@ def predict(model, df):
 
 model = get_model()
 
+
 st.title("Insurance Charges Prediction App")
+st.markdown("Enter your personal details to get a prediction of your insurance\
+        charges. This is a simple app showcasing the abilities of the PyCaret\
+        regression module, based on Streamlit. For more information visit the\
+        [Simplifying Machine Learning with PyCaret]\
+        (https://leanpub.com/pycaretbook/) book website.")
 
 form = st.form("charges")
 age = form.number_input('Age', min_value=1, max_value=100, value=25)
-sex = form.selectbox('Sex', ['male', 'female'])
-bmi = form.number_input('BMI', min_value=10, max_value=50, value=10)
-children = form.number_input('Children', min_value=0, max_value=10, value=0)
-
+sex = form.radio('Sex', ['Male', 'Female'])
+bmi = form.number_input('BMI', min_value=10.0, max_value=50.0, value=20.0)
+children = form.slider('Children', min_value=0, max_value=10, value=0)
+region_list = ['Southwest', 'Northwest', 'Northeast', 'Southeast']
+region = form.selectbox('Region', region_list)
 if form.checkbox('Smoker'):
     smoker = 'yes'
 else:
     smoker = 'no'
-
-region_list = ['southwest', 'northwest', 'northeast', 'southeast']
-region = form.selectbox('Region', region_list)
+    
 predict_button = form.form_submit_button('Predict')
 
-
-input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children,
-              'smoker' : smoker, 'region' : region}
+input_dict = {'age' : age, 'sex' : sex.lower(), 'bmi' : bmi, 'children' : children,
+              'smoker' : smoker, 'region' : region.lower()}
 input_df = pd.DataFrame([input_dict])
 
 if predict_button:
